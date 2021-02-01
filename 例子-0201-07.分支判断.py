d424645dfc95875d2474fa1c6a6979dbac6e09cc
blackplusy <osimeno@163.com>
2021-02-01 15:36:38 +0800

    Add files via upload

---
+++ 例子-0201-07.分支判断.py
@@ -0,0 +1,28 @@
+#coding=utf-8
+#单分支
+'''
+name=input("请输入您大表哥的名字:")
+
+if name=='蔡徐坤':
+    print('唱，跳，rap，篮球')
+'''
+#双分支
+'''
+name=input('为什么叫贝塔？')
+
+if name=='巧碧螺':
+    print('来点波关注不迷路')
+else:
+    print('舒克的飞机刷没了')
+'''
+#多分支
+name=input('请输入您喜欢的主播')
+if name=='醉叮当':
+    print('来，刷火箭！')
+elif name=='55开':
+    print('我没开挂！')
+elif name=='大司马':
+    print('正方形打野')
+else:
+    print('来，老铁们，刷波666')
+
