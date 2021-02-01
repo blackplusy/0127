a9fa87078a0cdac2b887cf7279f92e8d57f9cde7
blackplusy <osimeno@163.com>
2021-02-01 17:15:05 +0800

    Add files via upload

---
+++ 例子-0201-08.for循环.py
@@ -0,0 +1,26 @@
+#coding=utf-8
+#字符串
+'''
+for i in 'helloworld':
+    print(i)
+'''
+#列表
+l1=[1,2,'heygor','ladeng']
+for a in l1:
+    print(a)
+print('*'*10)
+#函数
+#内置函数 range() 范围
+#range(10)   0-9
+#range(1,10) 1-9
+'''
+for i in range(10):
+    print(i)
+
+print('*'*10)
+
+for i in range(1,10):
+    print(i)
+'''
+for i  in  range(-3,4):
+    print(i)
