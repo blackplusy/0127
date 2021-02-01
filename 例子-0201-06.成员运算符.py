7c7f09fcb75e1ef30c0a30b5b47e62d95f9cb153
blackplusy <osimeno@163.com>
2021-02-01 14:24:26 +0800

    Add files via upload

---
+++ 例子-0201-06.成员运算符.py
@@ -0,0 +1,16 @@
+#coding=utf-8
+#字符串
+
+name='helloworld'
+
+for i in name:
+    print(i)
+
+#列表
+l=['kobe','rose','malong']
+for u in l:
+    print(u)
+
+#判断
+if 'e' in name:
+    print('e is here!!!')
